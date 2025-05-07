b017771f6e7c29e6c9c6f1f940c70ce09617f654
Ishmam Abir | イシュマム・アビル <ishmam130@gmail.com>
2025-05-07 16:31:19 +0900

    udpate: kodensya description (en & ja)

--- src/components/AppJobExperience.vue
+++ src/components/AppJobExperience.vue
@@ -112,22 +112,37 @@ export default {
           description_en:
             `
             <P>
-            Kodensha Co., Ltd., established in 1949 and headquartered in Miyazaki, Japan, has long been recognized for its expertise in electrical facility construction and the sale of industrial electrical equipment. In recent years, the company has expanded into the IT sector, offering innovative software solutions to support Digital Transformation (DX). By integrating advanced technology with its engineering roots, Kodensha is committed to driving efficiency, safety, and modernization across industries.
+            Kodensha Co., Ltd., established in 1949 and headquartered in Miyazaki, Japan, has long been recognized for its expertise in electrical facility construction and the sale of industrial electrical equipment.
+            <br>
+            In recent years, the company has expanded into the IT sector, offering innovative software solutions to support Digital Transformation (DX). By integrating advanced technology with its engineering roots, Kodensha is committed to driving efficiency, safety, and modernization across industries.
             <P>
             <h4>Job Description</h4>
             <p>
               At Kodensya, i was responsible for complete lifecycle of database design, designing, developing & maintaining their system software, upgrading their old softwares and create modularity with their new software.
+              <br>
               I have implemented clean architecture using golang on this project without using any framework. Also, applied multi-module architecture so that old softwares can easily merged as a module to the current system as well as new modules can be introduced easily without modifying the whole systems in the backend. 
               Also, I designed the frontend using Vuetify 3, a vueJs component framework and updated it to be user friendly.
+              <br>
               This web aplicaaation is currently in live and our company and several other customers are using it, and we are also developing new features and modules that will scale this project.  
             </p>
-              <h4>Technology I Used</h4>
+              <h4>Technology Used</h4>
               <P>
                 Technology I got used here to level up my knowledge are given below
+                
+                <h6>Backend Responsibilities</h6>
                 <ul>
                   <li>
-                    <b>Golang</b> as backend.
-                  </li>  
+                    R&D for new web application using <b>Golang</b> as backend.
+                  </li>
+                  <li>
+                    Database design & ER diagram creation for the backend while keeping in mind the modularity of the system.
+                  </li>
+                  <li>
+                    Designed and implemented <a href="https://github.com/IshmamAbir/bakery-multimodule-golang" target="_blank">Multi Module Architecture</a> in the backend.
+                  </li>
+                  <li>
+                    Applied <a href="https://github.com/IshmamAbir/Clean-Architecture-Go-Postgres" target="_blank">Clean Architecture</a> in the backend without using any kind of framework.
+                  </li>
                   <li>
                     <a href="https://gorm.io/index.html" target="_blank">Gorm</a> as the Full-Featured ORM library for golang.
                   </li>
@@ -135,13 +150,143 @@ export default {
                     <a href="https://github.com/gorilla/mux" target="_blank">Gorilla mux</a> as HTTP router.
                   </li>
                   <li>
-                    
+                    Implemented <a href="https://github.com/IshmamAbir/Clean-Architecture-Go-Postgres/tree/swagger-golang" target="_blank">Go Swagger</a> for API documentations and command line formatter and documentations generator in multi module project
+                  </li>
+                  <li>
+                    Optimised Uploaded image size by compressing to smaller size, adding watermark to uploaded image. Added a demo <a href="https://github.com/IshmamAbir/photo-optimize-go" target="_blank">here</a>
+                  </li>
+                  <li>
+                    Use of <a href="https://github.com/IshmamAbir/TimescaleDB-Golang" target="_blank">timescaledb</a> for time series data.
+                  </li>
+                  <li>
+                    Implemented <a href="https://github.com/IshmamAbir/Go-Service_Level_Transaction" target="_blank">service level transaction</a> for data consistency in database.
+                  </li>
+                </ul>
+                <h6>Frontend Responsibilities</h6>
+                <ul>
+                  <li>
+                    Responsible for Vue version upgrade from Vue 2 to Vue 3.
+                  </li>
+                  <li>
+                    Responsible for implementing Vuetify 3 on the new project. 
+                  </li>
+                  <li>
+                    Introduced to <a href="https://pinia.vuejs.org/" target="_blank">pinia</a>, a state management library for Vue in the new project.
+                  </li>
+                  <li>
+                    Introduced <a href="https://vite.dev/" target="_blank">vite</a>, a modern, fast, and lightweight build tool for Vue which is faster and more efficient and loads faster then previous softwares.
+                  </li>
+                  <li>
+                    Implemented our own pdf reader component to read & control pdf actions in the application. 
+                  </li>
+                  <li>
+                    Created & implemented a <a href="https://github.com/IshmamAbir/vue-dxf-viewer" target="_blank">dxf file(.DXF) viewer</a> to view Autocad files & control the layers of the file in the web.
+                  </li>
+                  <li>
+                    other libraries like vue-router, vue3-toastify, vue-chartjs, v-phone-input, vue-qrcode, vue-uuid implement in the frontend. 
+                  </li>
+                </ul>
+                
+                <h6>Server Responsibilities</h6>
+                <ul>
+                  <li>
+                    Responsible to create a <a href="https://dev.to/ishmam_abir/set-up-your-own-local-gitlab-server-self-hosted-gitlab-4d1" target="_blank">Gitlab local server</a> to maintain version controlling of the company's codebase & dockerize to company server.
+                  </li>
+                  <li>
+                    Maintaining database, redis build file container at company's server.
                   </li>
                 </ul>
               </p>
             `,
           description_ja:
-            "",
+            `
+            <P>
+              株式会社コデンシャは、1949年に設立され、日本の宮崎市に本社を構え、長年にわたり電気設備工事および産業用電気機器の販売において高い専門性を誇っています。
+              <br>
+              近年では、IT分野にも進出し、デジタルトランスフォーメーション（DX）を支援する革新的なソフトウェアソリューションを提供しています。先進技術とエンジニアリングのノウハウを融合させることで、業界全体の効率化、安全性、近代化の推進に取り組んでいます。
+            </P>
+            <h4>職務内容</h4>
+            <p>
+              コデンシャでは、データベース設計のライフサイクル全体を担当し、システムソフトウェアの設計、開発、保守、旧ソフトウェアのアップグレード、新ソフトウェアとのモジュール化を行いました。
+              <br>
+              このプロジェクトでは、フレームワークを使用せずにGolangを使ってクリーンアーキテクチャを実装しました。また、マルチモジュールアーキテクチャを適用し、既存の旧ソフトウェアをモジュールとして統合しやすくし、新しいモジュールも既存のシステム全体を修正せずに導入できる構成にしました。
+              フロントエンドはVuetify 3（Vue.jsのコンポーネントフレームワーク）を使用して設計し、ユーザーフレンドリーなUIへと更新しました。
+              <br>
+              このWebアプリケーションは現在稼働中で、当社および複数の顧客が使用しており、さらにスケーラビリティを高めるための新機能やモジュールの開発も進めています。
+            </p>
+            <h4>使用技術</h4>
+            <P>
+              このプロジェクトで習得・活用した技術は以下の通りです。
+
+              <h6>バックエンドの担当範囲</h6>
+              <ul>
+                <li>
+                  バックエンドとして<b>Golang</b>を用いた新規WebアプリケーションのR&D。
+                </li>
+                <li>
+                  システムのモジュール性を考慮したデータベース設計およびER図の作成。
+                </li>
+                <li>
+                  バックエンドに<a href="https://github.com/IshmamAbir/bakery-multimodule-golang" target="_blank">マルチモジュールアーキテクチャ</a>を設計・実装。
+                </li>
+                <li>
+                  フレームワークを使わずに<a href="https://github.com/IshmamAbir/Clean-Architecture-Go-Postgres" target="_blank">クリーンアーキテクチャ</a>を適用。
+                </li>
+                <li>
+                  Golang用の高機能ORMライブラリである<a href="https://gorm.io/index.html" target="_blank">Gorm</a>を使用。
+                </li>
+                <li>
+                  HTTPルーターとして<a href="https://github.com/gorilla/mux" target="_blank">Gorilla mux</a>を使用。
+                </li>
+                <li>
+                  APIドキュメントとコマンドラインフォーマッター生成のために、マルチモジュールプロジェクト内で<a href="https://github.com/IshmamAbir/Clean-Architecture-Go-Postgres/tree/swagger-golang" target="_blank">Go Swagger</a>を実装。
+                </li>
+                <li>
+                  画像アップロード時に画像サイズを圧縮し、透かしを追加することで最適化。デモは<a href="https://github.com/IshmamAbir/photo-optimize-go" target="_blank">こちら</a>。
+                </li>
+                <li>
+                  時系列データ用に<a href="https://github.com/IshmamAbir/TimescaleDB-Golang" target="_blank">TimescaleDB</a>を使用。
+                </li>
+                <li>
+                  データベースの整合性を保つために<a href="https://github.com/IshmamAbir/Go-Service_Level_Transaction" target="_blank">サービスレベルトランザクション</a>を実装。
+                </li>
+              </ul>
+              <h6>フロントエンドの担当範囲</h6>
+              <ul>
+                <li>
+                  Vue 2 から Vue 3 へのバージョンアップを担当。
+                </li>
+                <li>
+                  新プロジェクトに Vuetify 3 を導入。
+                </li>
+                <li>
+                  新プロジェクトで Vue の状態管理ライブラリである<a href="https://pinia.vuejs.org/" target="_blank">Pinia</a>を導入。
+                </li>
+                <li>
+                  高速で軽量なビルドツール<a href="https://vite.dev/" target="_blank">Vite</a>を導入し、旧システムよりも高速な読み込みを実現。
+                </li>
+                <li>
+                  PDFの読み込みおよび操作制御のための独自PDFビューアコンポーネントを実装。
+                </li>
+                <li>
+                  AutoCADファイル(.DXF)の表示とレイヤー操作を可能にする<a href="https://github.com/IshmamAbir/vue-dxf-viewer" target="_blank">DXFビューア</a>を作成・導入。
+                </li>
+                <li>
+                  その他ライブラリ：vue-router、vue3-toastify、vue-chartjs、v-phone-input、vue-qrcode、vue-uuidなどをフロントエンドに実装。
+                </li>
+              </ul>
+
+              <h6>サーバー関連の担当範囲</h6>
+              <ul>
+                <li>
+                  会社のコードベースのバージョン管理とDocker化のために、<a href="https://dev.to/ishmam_abir/set-up-your-own-local-gitlab-server-self-hosted-gitlab-4d1" target="_blank">ローカルGitLabサーバー</a>を構築。
+                </li>
+                <li>
+                  会社サーバーでのデータベース・Redis・ビルドファイルコンテナの管理。
+                </li>
+              </ul>
+            </P>
+            `,
           imgUrl: "",
         },
         {
