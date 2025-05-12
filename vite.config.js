3ddb575fe642fb7b7babf56d15237c504b1174fa
Ishmam Abir | イシュマム・アビル <ishmam130@gmail.com>
2025-05-12 09:22:35 +0900

    update: base

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/Portfolio/",
   plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
