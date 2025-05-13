c38a250509f8ee5c79df7fa85885952f4d3439f7
Ishmam Abir | イシュマム・アビル <ishmam130@gmail.com>
2025-05-13 10:35:54 +0900

    add: axios

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
+import axios from "axios";
+import { createPinia } from "pinia";
 import { createApp } from "vue";
+import VueAxios from "vue-axios";
 import App from "./App.vue";
-import router from "./router";
 import i18n from "./includes/i18n";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
-import { createPinia } from "pinia";
+import router from "./router";
+
 loadFonts();
 
 createApp(App)
   .use(router)
   .use(vuetify)
+  .use(VueAxios, axios)
   .use(i18n)
   .use(createPinia())
   .mount("#app");
